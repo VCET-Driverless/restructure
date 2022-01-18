6d8f538c52cde4ca629753bd5fc0b905a61dc663
Chaitanya Patil <chaitanya.191423104@vcet.edu.in>
2022-01-18 18:43:07 +0530

    adding logging file

--- src/system_manager/loger/logging.py
+++ src/system_manager/loger/logging.py
@@ -1,3 +1,27 @@
+import os
+import datetime
 
-def log():
-    pass
+
+class Log:
+    log_folder = "logs"
+    test_count = 0
+    today = datetime.datetime.now()
+
+    if not os.path.isdir(log_folder):
+        os.mkdir(log_folder)
+
+    day_folder = log_folder + "/" + \
+        str(today.day) + "-" + str(today.month) + "-" + str(today.year)
+
+    if not os.path.isdir(day_folder):
+        os.mkdir(day_folder)
+
+    while os.path.isfile(day_folder + "/" + "test" + str(test_count) + ".json"):
+        test_count = test_count + 1
+
+    log_file_name = day_folder + "/" + "test" + str(test_count)
+
+    f = open(log_file_name + ".json", "w+")
+
+    def filename(self):
+        return Log.f, Log.log_file_name
