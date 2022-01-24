5c07e3691772ada4d11abdb222762c8b6930077e
sharvin18 <dedhiasharvin@gmail.com>
2022-01-24 14:03:16 +0530

    resolving confilcts

--- src/run.py
+++ src/run.py
@@ -9,7 +9,6 @@ from system_manager.fault_manager.detect import check_fault
 from system_manager.loger.logging import log
 
 def main():
-    check_fault()
     while True:
         # perception
         frame = None
