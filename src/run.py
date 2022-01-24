d9bdec46a1cd96bffbe5183f61f0a813d9cbc7e1
sharvin18 <dedhiasharvin@gmail.com>
2022-01-24 15:35:15 +0530

    update: structure of run.py

--- src/run.py
+++ src/run.py
@@ -4,67 +4,73 @@ from asyncio.proactor_events import constants
 from multiprocessing import Process,Queue,Pipe
 
 # System imports
-from perception.perception import Perception
+from perception.perceive import Perceive
+from perception.detect import Detect
 from planning.path_plan import Planning
 from control.control import Control
 from system_manager.setup import Setup
 from system_manager.fault_manager import Fault
-from system_manager.constants import Constants
-
-# fault and log will be called in each class individually at required places.
-# from system_manager.fault_manager.detect import check_fault
-# from system_manager.loger.logging import Log
+from system_manager.loger.logging import Log
 
 
 def main():
 
 	# Declaring objects(instances variables) of each core class
-	constants = Constants()
-	constants.set_constants()
-	setup = Setup(constants)
+	setup = Setup()
 	setup.setup_driver()
-	perception = Perception()
-	path_plan = Planning(constants)
-	control = Control(constants)
+	perception = Perceive()
+	detect = Detect()
+	path_plan = Planning()
+	control = Control()
+	log = Log()
 	fault = Fault()
 	
 	# Raise error w.r.t hardware or software(eg: cv2 port not found, serial port not found, log file name already exists, etc)
 	fault.check_fault(setup, perception, path_plan, control)
 	
 	# Declaring Shared memory variables for data transfer
-	frame_queue = Queue()           # used by perception
-	detections_queue = Queue()      # use by percetion and path_plan
-	top_view_queue = Queue()        # use by perception and path_plan
-	path_queue = Queue()            # used by path_plan and control
+	frame_queue = Queue()                        # used by perception and path_plan 
+	darknet_image_queue = Queue()                # used by perception and detect
+	detections_queue = Queue()                   # use by percetion and path_plan
+	top_view_queue = Queue()                     # use by perception and path_plan
+	top_view_blue_coordinates_queue = Queue()    # used by detect and pathplan
+	top_view_orange_coordinates_queue = Queue()  # used by detect and pathplan
+	log_queue = Queue()                          # used by path plan
 	
 	# Declaring Shared memory variables for terminating each process gracefully
 	p1_parent, p1_child = Pipe()  # shared by perception and plan
 	p2_parent, p2_child = Pipe()  # shared by plan and control
 	
 	# Declare all the processes
-	process1 = Process(target=perception.perception_driver,
+	process1 = Process(target=perception.video_capture,
 							args=(
 								setup, 
 								frame_queue,
-								detections_queue,
+								darknet_image_queue,
 								top_view_queue,
-								p1_parent,
-							))
-							
-	process2 = Process(target=path_plan.path_plan_driver,
-							args=(
-								setup,
-								top_view_queue,
-								path_queue,
-								p1_child,
-								p2_parent,	
-							))
-							
-	process3 = Process(target=control.control_driver,
-							args=(
-								setup,
-								path_queue,
 								p2_child,
+							))
+	
+	process2 = Process(target=detect.detect,
+							args=(
+								setup, 
+        						darknet_image_queue, 
+								detections_queue,
+        						top_view_blue_coordinates_queue,
+								top_view_orange_coordinates_queue, 
+        						p1_child,
+              					p2_parent
+							))
+ 
+	process3 = Process(target=path_plan.path_plan_driver,
+							args=(	
+								setup, 
+								frame_queue, 
+        						top_view_queue, 
+              					top_view_blue_coordinates_queue, 
+                   				top_view_orange_coordinates_queue,
+								log_queue,
+        						p1_parent
 							))
 	
 	try:						
@@ -78,6 +84,9 @@ def main():
 		process2.join()
 		process3.join()
 		
+		# Log and save data
+		log.log(log_queue)
+  
 		print("Execution Succesful")
 		
 	except:
