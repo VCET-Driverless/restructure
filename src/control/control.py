0e996d3c16c7775ab1d19cebaafaae7b05cb51ea
sharvin18 <dedhiasharvin@gmail.com>
2022-01-24 14:49:38 +0530

    integration with path plan - Fixed many issues - Control inherits from Constants - returned necessary variables

--- src/control/control.py
+++ src/control/control.py
@@ -4,23 +4,20 @@ import numpy as np
 import math
 import cv2
 import time
+import sys
+sys.path.append('../system_manager')
 
 # System imports
 from pure_pursuit import Pure_Pursuit
+from constants import Constants
 
 
-#sending top view image to the control module is left  -> Done, sent to control.
-#top view image is needed to draw pure pursuit path
-
-class Control:
+class Control(Constants):
     
-    def __init__(self, constants):
+    def __init__(self):
         
-        self.P = constants.P
+        super().__init__()
         self.limit_frames = 5
-        self.rate = constants.RATE
-        self.MS = constants.MS
-        self.top_view_img_dim = constants.TOP_VIEW_IMAGE_DIMESNION
         
     def send_steer_angle(angle):
         """
@@ -64,24 +61,27 @@ class Control:
         if (angle > 0):
             return -1 * (90 - angle)
         return (90 + angle)
+    
+    def stop_car(self, setup):
+        if setup.ARDUINO_CONNECTED:
+                setup.s.write(str('c').encode())
+                       
 
-    def control(self, setup, path_queue, constants, top_view_image, p2_child):
+    def control(self, setup, path, steering, prev_time, top_view_image):
         
-        pp = Pure_Pursuit(constants)
+        pp = Pure_Pursuit()
         angle = 0
-        steering = '4'
         angle_limit = [0]*self.limit_frames
-        prev_time_my = time.time()
-                    
-        lines = path_queue.get()
+        path_list = path
+        serial_writen_now = False
 
         if (setup.args.controller == 0):
             # encode signal for steering control(old controller)
-            angle = Control.angle(self,lines[0],lines[1])
+            angle = Control.angle(self,path_list[0],path_list[1])
             angle = math.floor(angle)
         elif (setup.args.controller == 1):
             # encode signal for steering control(new controller)
-            angle, top_image = pp.pure_pursuit(self,lines,top_view_image)
+            angle, top_view_image = pp.pure_pursuit(self, path_list, top_view_image)
             angle = math.floor(angle)
 
         angle_limit.append(angle)
@@ -89,14 +89,16 @@ class Control:
         angle_send = Control.send_steer_angle((sum(angle_limit))//self.limit_frames)
         st_ang = self.P * (sum(angle_limit)) // self.limit_frames
 
-        if (time.time() - prev_time_my >= self.MS):
-            prev_time_my = time.time()
+        if (time.time() - prev_time >= self.MS):
+            prev_time = time.time()
             if (setup.ARDUINO_CONNECTED):
                 serial_data = st_ang
                 serial_writen_now = True
                 setup.s.write(str(serial_data).encode())
+        else:
+            serial_writen_now = False
 
-            # Prevents Arduino buffer overlfow,
+        # Prevents Arduino buffer overlfow,
         if (steering != angle_send):
             if (setup.ARDUINO_CONNECTED):
                 '''s.write(str.encode(angle_a))'''
@@ -104,15 +106,5 @@ class Control:
             steering = angle_send
             print('updated', angle_send)
 
-        if not setup.args.dont_show:
-            top_image = cv2.resize(top_image, (2 * self.top_view_img_dim[0],
-                                               2 * self.top_view_img_dim[1]))
-            cv2.imshow('top_view', top_image)
-
-        if cv2.waitKey(2) == 27:
-            if setup.ARDUINO_CONNECTED:
-                setup.s.write(str('c').encode())
-
-            # This will be added later in wip branch
-            # if p2_child.recv()==False:
-            #     break
+        return steering, st_ang, serial_writen_now, prev_time, top_view_image
+            
